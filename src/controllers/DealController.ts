a7520f45eff972896d18057f98dc8eefb4875854
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-29 21:47:22 -0300

    Add route to store deal

---
+++ src/controllers/DealController.ts
@@ -0,0 +1,27 @@
+import { Response } from 'express'
+import { BaseController, NotFoundError } from '@cig-platform/core'
+import { AuthenticatedRequest } from '@cig-platform/types'
+
+import DealAggregator from '@Aggregators/DealAggregator'
+
+class DealController {
+  constructor() {
+    this.store = this.store.bind(this)
+  }
+
+  @BaseController.errorHandler()
+  async store(req: AuthenticatedRequest, res: Response) {
+    const user = req.user
+    const merchant = req.merchant
+    const advertisingId = req.params.advertisingId
+    const breederId = req.params.breederId
+
+    if (!user || !merchant) throw new NotFoundError()
+
+    const deal = await DealAggregator.registerDeal(breederId, advertisingId, merchant.id)
+
+    return BaseController.successResponse(res, { deal })
+  }
+}
+
+export default new DealController()
