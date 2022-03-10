33424d636f36d45efc67f848d0e8ed824528bb94
Eduardo Moreira <contato@eduardoem.com.br>
2022-03-10 18:28:27 -0300

    Remove advertisings route

--- src/docs/index.ts
+++ src/docs/index.ts
@@ -1,11 +1,9 @@
 import questionDocs from './question'
 import dealDocs from './deal'
 import favoriteDocs from './favorite'
-import advertisingDocs from './advertising'
 
 export default {
   ...questionDocs,
   ...dealDocs,
   ...favoriteDocs,
-  ...advertisingDocs,
 }
