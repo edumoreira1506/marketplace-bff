6c8ae41469efd7dc97a012b489a9000db2878627
Eduardo Moreira <contato@eduardoem.com.br>
2022-03-10 20:07:08 -0300

    Chicken 707 (#17)
    * Bump @cig-platform/core to 0.8.1
    
    * Update withTokenAuthorization
    
    * Bump @cig-platform/core to 0.8.2
    
    * Update docs

---
+++ src/clients/AuthBffClient.ts
@@ -0,0 +1,4 @@
+import AuthBffClient from '@cig-platform/auth-bff-client'
+import { AUTH_BFF_URL } from '@Constants/url'
+
+export default new AuthBffClient(AUTH_BFF_URL)
--- src/docs/deal.ts
+++ src/docs/deal.ts
@@ -1,11 +1,14 @@
 import { createDoc } from '@cig-platform/docs'
 
+import { storeDealSchema } from '@Schemas/DealSchemas'
+
 export default {
   ...createDoc('/breeders/{breederId}/poultries/{poultryId}/advertisings/{advertisingId}/deals', ['Deals'], [
     {
       method: 'post',
       title: 'Register deal',
       headerParams: [{ type: 'string', name: 'X-Cig-Token' }],
+      objectSchema: storeDealSchema
     },
   ], {
     pathVariables: [
