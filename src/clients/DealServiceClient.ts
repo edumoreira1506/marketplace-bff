b5e8f409c1efcc811de445e8c8ee46bdd046ed06
edumoreira1506 <contato@eduardoem.com.br>
2022-09-10 16:37:43 -0300

    Add api-keys

--- src/clients/DealServiceClient.ts
+++ src/clients/DealServiceClient.ts
@@ -1,4 +1,5 @@
 import { DealServiceClient } from '@cig-platform/core'
+import { DEAL_SERVICE_API_KEY } from '@Constants/api-keys'
 import { DEAL_SERVICE_URL } from '@Constants/url'
 
-export default new DealServiceClient(DEAL_SERVICE_URL)
+export default new DealServiceClient(DEAL_SERVICE_URL, DEAL_SERVICE_API_KEY)
